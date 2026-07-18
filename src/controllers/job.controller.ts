2ce43d5a760f9b6d88d890b22cc3334c1821c233
SinghRohan333 <rohansingh12345602042@gmail.com>
2026-07-18 20:59:12 +0600

    controller: job controller added

---
+++ src/controllers/job.controller.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from "express";
+import {
+  createJob,
+  getJobs,
+  getJobById,
+  getSimilarJobs,
+  updateJob,
+  deleteJob,
+} from "../services/job.service";
+import { JobQueryInput } from "../validators/job.schema";
+
+export async function createJobHandler(req: Request, res: Response) {
+  const job = await createJob(req.user!.id, req.body);
+  res.status(201).json({ job });
+}
+
+export async function listJobsHandler(req: Request, res: Response) {
+  const query = req.validatedQuery as JobQueryInput;
+  const result = await getJobs(query, { restrictActive: true });
+  res.status(200).json(result);
+}
+
+export async function myJobsHandler(req: Request, res: Response) {
+  const query = req.validatedQuery as JobQueryInput;
+  const result = await getJobs(query, {
+    restrictActive: false,
+    employerId: req.user!.id,
+  });
+  res.status(200).json(result);
+}
+
+export async function getJobHandler(req: Request, res: Response) {
+  const job = await getJobById(req.params.id as string, req.user);
+  const similar = await getSimilarJobs(job);
+  res.status(200).json({ job, similar });
+}
+
+export async function updateJobHandler(req: Request, res: Response) {
+  const job = await updateJob(req.params.id as string, req.user!, req.body);
+  res.status(200).json({ job });
+}
+
+export async function deleteJobHandler(req: Request, res: Response) {
+  await deleteJob(req.params.id as string, req.user!);
+  res.status(200).json({ message: "Job deleted" });
+}

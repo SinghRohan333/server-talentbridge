01acf0ef92fcf80166f74f0bb833df644eb632bc
SinghRohan333 <rohansingh12345602042@gmail.com>
2026-07-19 22:17:42 +0600

    application route updated

--- src/routes/application.routes.ts
+++ src/routes/application.routes.ts
@@ -3,20 +3,42 @@ import { requireAuth } from "../middleware/auth";
 import { requireRole } from "../middleware/requireRole";
 import { validate } from "../middleware/validate";
 import { asyncHandler } from "../utils/asyncHandler";
-import { applyToJobSchema } from "../validators/application.schema";
+import {
+  applyToJobSchema,
+  updateApplicationStatusSchema,
+} from "../validators/application.schema";
 import {
   applyToJobHandler,
   listMyApplicationsHandler,
+  getApplicationsForJobHandler,
+  updateApplicationStatusHandler,
 } from "../controllers/application.controller";
 
 const router = Router();
-router.use(requireAuth, requireRole("seeker"));
+router.use(requireAuth);
 
-router.get("/mine", asyncHandler(listMyApplicationsHandler));
+router.get(
+  "/mine",
+  requireRole("seeker"),
+  asyncHandler(listMyApplicationsHandler),
+);
 router.post(
   "/:jobId",
+  requireRole("seeker"),
   validate(applyToJobSchema),
   asyncHandler(applyToJobHandler),
+);
+
+router.get(
+  "/job/:jobId",
+  requireRole("employer"),
+  asyncHandler(getApplicationsForJobHandler),
+);
+router.patch(
+  "/:id/status",
+  requireRole("employer"),
+  validate(updateApplicationStatusSchema),
+  asyncHandler(updateApplicationStatusHandler),
 );
 
 export default router;
